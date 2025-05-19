63b8a0dac0504d3e35ad126dd085a0cef1f37c09
Thomas Lucas <t.lucas@lienas.de>
2025-05-19 12:47:52 +0200

    again QA with FLAKE8

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -5,6 +5,7 @@ from .query_base import QueryBase
 # from the `sql_execution` module
 from .sql_execution import QueryMixin
 
+
 # Define a subclass of QueryBase
 # called Employee
 class Employee(QueryBase, QueryMixin):
@@ -18,31 +19,30 @@ class Employee(QueryBase, QueryMixin):
     # This method should return a list of tuples
     # from an sql execution
     def names(self):
-        
+
         # Query 3
         # Write an SQL query
-        # that selects two columns 
+        # that selects two columns
         # 1. The employee's full name
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
         sql_query = """
-                    SELECT 
+                    SELECT
                         CONCAT(first_name,' ', last_name) as full_name,
                         employee_id
                     FROM
                        employee
                     """
-        
+
         return self.query(sql_query)
-    
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     def username(self, id):
-        
+
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -54,9 +54,8 @@ class Employee(QueryBase, QueryMixin):
                     FROM employee
                     WHERE employee_id = {id}
                     """
-        
-        return self.query(sql_query)
 
+        return self.query(sql_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -65,7 +64,7 @@ class Employee(QueryBase, QueryMixin):
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
+
     def model_data(self, id):
 
         sql_query = f"""
@@ -76,5 +75,5 @@ class Employee(QueryBase, QueryMixin):
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                     """
-                
+
         return self.pandas_query(sql_query)
